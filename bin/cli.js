19edbb015f19ba03cb4a5bf7fc2dd42924806afb
devwitheasy@gamil.com <DevWithEasy>
2024-09-05 15:44:42 +0600

    cli update

--- bin/cli.js
+++ bin/cli.js
@@ -1,56 +1,5 @@
 #!/usr/bin/env node
 
-// const { execSync } = require('child_process');
-// const readline = require('readline');
-
-// const runCommand = command => {
-//     try {
-//         execSync(`${command}`, { stdio: 'inherit' });
-//     } catch (error) {
-//         console.error(`Failed to execute ${command}`);
-//         return false;
-//     }
-//     return true;
-// };
-
-// const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// });
-
-// const askProjectName = () => {
-//     return new Promise(resolve => {
-//         rl.question('Please enter the project name: ', (name) => {
-//             rl.close();
-//             resolve(name);
-//         });
-//     });
-// };
-
-// const main = async () => {
-//     let repoName = process.argv[2];
-
-//     if (!repoName) {
-//         repoName = await askProjectName();
-//     }
-
-//     const gitCheckoutCommand = `git clone --depth 1 https://github.com/DevWithEasy/express-mern-x ${repoName}`;
-//     const installDepsCommand = `cd ${repoName} && npm install`;
-
-//     console.log('Creating project...');
-//     const checkedout = runCommand(gitCheckoutCommand);
-//     if (!checkedout) process.exit(-1);
-
-//     console.log('Installing dependencies...');
-//     const installed = runCommand(installDepsCommand);
-//     if (!installed) process.exit(-1);
-
-//     console.log('Project setup is complete!');
-//     console.log(`cd ${repoName} && npm start`);
-// };
-
-// main();
-
 const { execSync } = require('child_process');
 const readline = require('readline');
 const fs = require('fs');
@@ -125,9 +74,13 @@ const main = () => {
 
             console.log('Project setup is complete!');
             console.log(`cd ${repoName} && npm start`);
+
+            // Exit the process when setup is complete
+            process.exit(0);
         })
         .catch((error) => {
             console.error(error);
+            process.exit(1); // Exit with an error code
         });
 };
 
