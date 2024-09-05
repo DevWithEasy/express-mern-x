093513f78436b87b9728c86797dcd4e44c1365a3
devwitheasy@gamil.com <DevWithEasy>
2024-09-05 15:40:16 +0600

    update cli

--- bin/cli.js
+++ bin/cli.js
@@ -1,9 +1,62 @@
 #!/usr/bin/env node
 
+// const { execSync } = require('child_process');
+// const readline = require('readline');
+
+// const runCommand = command => {
+//     try {
+//         execSync(`${command}`, { stdio: 'inherit' });
+//     } catch (error) {
+//         console.error(`Failed to execute ${command}`);
+//         return false;
+//     }
+//     return true;
+// };
+
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+// const askProjectName = () => {
+//     return new Promise(resolve => {
+//         rl.question('Please enter the project name: ', (name) => {
+//             rl.close();
+//             resolve(name);
+//         });
+//     });
+// };
+
+// const main = async () => {
+//     let repoName = process.argv[2];
+
+//     if (!repoName) {
+//         repoName = await askProjectName();
+//     }
+
+//     const gitCheckoutCommand = `git clone --depth 1 https://github.com/DevWithEasy/express-mern-x ${repoName}`;
+//     const installDepsCommand = `cd ${repoName} && npm install`;
+
+//     console.log('Creating project...');
+//     const checkedout = runCommand(gitCheckoutCommand);
+//     if (!checkedout) process.exit(-1);
+
+//     console.log('Installing dependencies...');
+//     const installed = runCommand(installDepsCommand);
+//     if (!installed) process.exit(-1);
+
+//     console.log('Project setup is complete!');
+//     console.log(`cd ${repoName} && npm start`);
+// };
+
+// main();
+
 const { execSync } = require('child_process');
 const readline = require('readline');
+const fs = require('fs');
+const path = require('path');
 
-const runCommand = command => {
+const runCommand = (command) => {
     try {
         execSync(`${command}`, { stdio: 'inherit' });
     } catch (error) {
@@ -19,7 +72,7 @@ const rl = readline.createInterface({
 });
 
 const askProjectName = () => {
-    return new Promise(resolve => {
+    return new Promise((resolve) => {
         rl.question('Please enter the project name: ', (name) => {
             rl.close();
             resolve(name);
@@ -27,27 +80,55 @@ const askProjectName = () => {
     });
 };
 
-const main = async () => {
+const deleteFolderRecursive = (folderPath) => {
+    if (fs.existsSync(folderPath)) {
+        fs.readdirSync(folderPath).forEach((file) => {
+            const curPath = path.join(folderPath, file);
+            if (fs.lstatSync(curPath).isDirectory()) {
+                deleteFolderRecursive(curPath);
+            } else {
+                fs.unlinkSync(curPath);
+            }
+        });
+        fs.rmdirSync(folderPath);
+    }
+};
+
+const main = () => {
     let repoName = process.argv[2];
 
-    // If no project name is provided, ask for it
-    if (!repoName) {
-        repoName = await askProjectName();
-    }
+    const getRepoName = () => {
+        if (!repoName) {
+            return askProjectName();
+        }
+        return Promise.resolve(repoName);
+    };
 
-    const gitCheckoutCommand = `git clone --depth 1 https://github.com/DevWithEasy/express-mern-x ${repoName}`;
-    const installDepsCommand = `cd ${repoName} && npm install`;
+    getRepoName()
+        .then((name) => {
+            repoName = name;
+            const gitCheckoutCommand = `git clone --depth 1 https://github.com/DevWithEasy/express-mern-x ${repoName}`;
+            const installDepsCommand = `cd ${repoName} && npm install`;
 
-    console.log('Creating project...');
-    const checkedout = runCommand(gitCheckoutCommand);
-    if (!checkedout) process.exit(-1);
+            console.log('Creating project...');
+            const checkedout = runCommand(gitCheckoutCommand);
+            if (!checkedout) process.exit(-1);
 
-    console.log('Installing dependencies...');
-    const installed = runCommand(installDepsCommand);
-    if (!installed) process.exit(-1);
+            // Remove the bin folder after cloning
+            const binFolderPath = path.join(repoName, 'bin');
+            console.log('Removing bin folder...');
+            deleteFolderRecursive(binFolderPath);
 
-    console.log('Project setup is complete!');
-    console.log(`cd ${repoName} && npm start`);
+            console.log('Installing dependencies...');
+            const installed = runCommand(installDepsCommand);
+            if (!installed) process.exit(-1);
+
+            console.log('Project setup is complete!');
+            console.log(`cd ${repoName} && npm start`);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
 };
 
 main();
