906562879dffd257d79a08d4aa38ade9bff432b4
devwitheasy@gamil.com <DevWithEasy>
2024-09-05 12:11:33 +0600

    asking project name

--- bin/cli.js
+++ bin/cli.js
@@ -1,29 +1,53 @@
 #!/usr/bin/env node
 
-const {execSync} = require('child_process');
+const { execSync } = require('child_process');
+const readline = require('readline');
 
-const runCommand = command =>{
+const runCommand = command => {
     try {
-        execSync(`${command}` ,{stdio : 'inherit'});
+        execSync(`${command}`, { stdio: 'inherit' });
     } catch (error) {
-        console.error(`Failed to execute ${command}`)
+        console.error(`Failed to execute ${command}`);
         return false;
     }
     return true;
-}
+};
 
-const repoName = process.argv[2];
-const gitCheckoutCommand = `git clone --depth 1 https://github.com/DevWithEasy/express-mern-x ${repoName}`;
-const installDepsCommand = `cd ${repoName} && npm install`;
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-console.log('Creating project...')
-const checkedout = runCommand(gitCheckoutCommand);
-if (!checkedout) process.exit(-1);
+const askProjectName = () => {
+    return new Promise(resolve => {
+        rl.question('Please enter the project name: ', (name) => {
+            rl.close();
+            resolve(name);
+        });
+    });
+};
 
-console.log('Installing dependencies...')
+const main = async () => {
+    let repoName = process.argv[2];
 
-const installed = runCommand(installDepsCommand);
-if (!installed) process.exit(-1);
+    // If no project name is provided, ask for it
+    if (!repoName) {
+        repoName = await askProjectName();
+    }
 
-console.log('Project setup is complete!');
-console.log('cd ${repoName} && npm install');
+    const gitCheckoutCommand = `git clone --depth 1 https://github.com/DevWithEasy/express-mern-x ${repoName}`;
+    const installDepsCommand = `cd ${repoName} && npm install`;
+
+    console.log('Creating project...');
+    const checkedout = runCommand(gitCheckoutCommand);
+    if (!checkedout) process.exit(-1);
+
+    console.log('Installing dependencies...');
+    const installed = runCommand(installDepsCommand);
+    if (!installed) process.exit(-1);
+
+    console.log('Project setup is complete!');
+    console.log(`cd ${repoName} && npm start`);
+};
+
+main();
